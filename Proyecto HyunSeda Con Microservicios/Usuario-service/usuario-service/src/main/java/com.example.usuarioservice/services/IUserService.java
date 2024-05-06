0c3a4829b90b0930184713ed810af3e4acf625fd
sharythvelasco <159739537+sharythvelasco@users.noreply.github.com>
2024-05-06 18:46:40 -0500

    Update IUserService.java
    documentacion

--- Proyecto HyunSeda Con Microservicios/Usuario-service/usuario-service/src/main/java/com.example.usuarioservice/services/IUserService.java
+++ Proyecto HyunSeda Con Microservicios/Usuario-service/usuario-service/src/main/java/com.example.usuarioservice/services/IUserService.java
@@ -1,6 +1,5 @@
 package com.example.usuarioservice.services;
 
-
 import com.example.usuarioservice.exceptions.ResourceNotFoundException;
 import com.example.usuarioservice.exceptions.UserDomainException;
 import com.example.usuarioservice.models.UserModel;
@@ -8,12 +7,62 @@ import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 
+/**
+ * Interfaz que define las operaciones disponibles para el servicio de usuarios.
+ */
 public interface IUserService {
+
+    /**
+     * Obtiene todos los usuarios.
+     * @return Una lista de objetos UserModel representando todos los usuarios.
+     */
     public ArrayList<UserModel> getUsers();
-    public UserModel save(UserModel newUser)throws UserDomainException;
-    public UserModel findById(Long id)throws ResourceNotFoundException;
+
+    /**
+     * Guarda un nuevo usuario.
+     * @param newUser El nuevo usuario a ser guardado.
+     * @return El usuario guardado.
+     * @throws UserDomainException Si ocurre un error relacionado con la lógica del dominio del usuario.
+     */
+    public UserModel save(UserModel newUser) throws UserDomainException;
+
+    /**
+     * Busca un usuario por su identificador único.
+     * @param id El identificador del usuario a buscar.
+     * @return El usuario encontrado.
+     * @throws ResourceNotFoundException Si el usuario no puede ser encontrado.
+     */
+    public UserModel findById(Long id) throws ResourceNotFoundException;
+
+    /**
+     * Busca un usuario por su nombre.
+     * @param name El nombre del usuario a buscar.
+     * @return El usuario encontrado, o null si no se encuentra.
+     */
     public UserModel findByName(String name);
-    public UserModel updateById(UserModel newUser, long id)throws UserDomainException,ResourceNotFoundException;
+
+    /**
+     * Actualiza un usuario existente por su identificador único.
+     * @param newUser El usuario con los nuevos datos.
+     * @param id El identificador del usuario a actualizar.
+     * @return El usuario actualizado.
+     * @throws UserDomainException Si ocurre un error relacionado con la lógica del dominio del usuario.
+     * @throws ResourceNotFoundException Si el usuario no puede ser encontrado.
+     */
+    public UserModel updateById(UserModel newUser, long id) throws UserDomainException, ResourceNotFoundException;
+
+    /**
+     * Elimina un usuario por su identificador único.
+     * @param id El identificador del usuario a eliminar.
+     * @return true si se eliminó correctamente, false de lo contrario.
+     */
     public boolean deleteById(Long id);
+
+    /**
+     * Valida las credenciales de usuario.
+     * @param username El nombre de usuario.
+     * @param password La contraseña de usuario.
+     * @return ResponseEntity con un booleano que indica si las credenciales son válidas o no.
+     */
     public ResponseEntity<Boolean> validateUserCredentials(String username, String password);
 }
